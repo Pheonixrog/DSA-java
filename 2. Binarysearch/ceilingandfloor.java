880c2672f4a49c21b7e512d50ff2c08f83ec082c
Rishabh Katiyar <rishabhasus9@gmail.com>
2023-12-10 23:24:40 +0530

    binary search ceiling anf floor question

---
+++ 2. Binarysearch/ceilingandfloor.java
@@ -0,0 +1,78 @@
+public class ceilingandfloor {
+
+    public static void main(String[] args) {
+        int[] a = { 1, 8, 15, 86, 99, 120 };
+        int b= a.length;
+        int target = 79;
+        System.out.println(ceiling(a, b,target));
+
+    }
+
+    public static int ceiling(int[] a, int b, int target) {
+        int start = 0;
+        int end = b - 1;
+
+        if (a[start] < a[end]) {
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if (a[mid] == target) {
+                    
+                    break;
+                } else if (a[mid] < target) {
+                    start = mid + 1;
+                    
+                } else {
+                    end = mid - 1;
+                    
+
+                }
+
+            }
+            if (start < b) {
+                System.out.println("Solution is " + start);
+                return start;
+            }
+           
+        }
+       
+        return 0;
+    }
+
+
+    public static int floor(int[] a, int b, int target) {
+        int start = 0;
+        int end = b - 1;
+
+        if (a[start] < a[end]) {
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if (a[mid] == target) {
+                    
+                    break;
+                } else if (a[mid] < target) {
+                    start = mid + 1;
+                    
+                } else {
+                    end = mid - 1;
+                    
+
+                }
+
+            }
+            if (start < b) {
+                System.out.println("Solution is " + start);
+                return start;
+            }
+           
+        }
+       
+        return 0;
+    }
+
+    
+
+}
