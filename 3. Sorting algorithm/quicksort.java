3195c494e4611a846042ef55fc4a6de8bbe4a544
Rishabh Katiyar <rishabhasus9@gmail.com>
2023-12-13 20:31:08 +0530

    quick sort completed

--- 3. Sorting algorithm/quicksort.java
+++ 3. Sorting algorithm/quicksort.java
@@ -1,3 +1,25 @@
+import java.util.Arrays;
+
 public class quicksort {
+    public static void main(String[] args) {
+        int[] arr = {1,4,2,3,5,0,6,7};
+        cyclicsort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        
+    }
+
+    public static void cyclicsort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+
+            while( arr[i] != i){
+                
+                int temp = arr[i];
+                arr[i] =arr[temp];
+                arr[temp] =temp;
+            }
+            
+        }
+    }
     
 }
