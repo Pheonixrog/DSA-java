f776b77829b94a5ded5779a08d713c509ac1fee1
Rishabh Katiyar <rishabhasus9@gmail.com>
2023-12-16 23:10:17 +0530

    update on LL

--- 5. Linked list/LL.java
+++ 5. Linked list/LL.java
@@ -233,16 +233,16 @@ public class LL {
     }
 
     // recursion reverse
-    private void reverse(Node node) {
-        if (node == tail) {
-            head = tail;
-            return;
-        }
-        reverse(node.next);
-        tail.next = node;
-        tail = node;
-        tail.next = null;
-    }
+    // private void reverse(Node node) {
+    //     if (node == tail) {
+    //         head = tail;
+    //         return;
+    //     }
+    //     reverse(node.next);
+    //     tail.next = node;
+    //     tail = node;
+    //     tail.next = null;
+    // }
 
     // in place reversal of linked list
     // google, microsoft, apple, amazon: https://leetcode.com/problems/reverse-linked-list/
