16395ef8f190b5106872972ae015f876e2d67aab
Rishabh Katiyar <rishabhasus9@gmail.com>
2023-12-17 21:04:53 +0530

    sum of digit and mul of digit , by recursion

---
+++ 4. recursion/sumofdigit.java
@@ -0,0 +1,32 @@
+public class sumofdigit {
+    public static void main(String[] args) {
+      int n = 3564; 
+      System.out.println(sumofDigit(n)); 
+      System.out.println(mulofDigit(n));
+
+    }
+
+    public static int sumofDigit(int n){
+        if(n==0){
+            return 0;
+        }
+
+        int rem = n%10;
+        n = n/10;
+
+        return rem + sumofDigit(n);
+
+    }
+
+    public static int mulofDigit(int n){
+        if(n==0){
+            return 1;
+        }
+
+        int rem = n%10;
+        n = n/10;
+
+        return rem * mulofDigit(n);
+
+    }
+}
