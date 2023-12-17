65e9672527a4b1f58e89d980ab250ac8f8ca67c1
Rishabh Katiyar <rishabhasus9@gmail.com>
2023-12-17 21:26:57 +0530

    palindrome and reverse of the number by recursion

--- 4. recursion/reverse.java
+++ 4. recursion/reverse.java
@@ -1,14 +1,33 @@
 public class reverse {
     public static void main(String[] args) {
-        int n = 5896;
+        int n = 151;
+        System.out.println(Reverse(n));
+        System.out.println(palindrome(Reverse(n), n));
+        
 
     }
+     
+    public static int sum = 0;
 
     public static int Reverse(int n){
         if(n==0){
-            return 0;
+            return sum;
         }
+       
+        int rem = n%10;
+        sum = sum*10 + rem;
+        n = n/10;
+
+
+        return Reverse(n);
 
     }
     
+    public static boolean palindrome(int a , int b) {
+        if(a==b){
+            return true;
+        }
+        return false;
+        
+    }
 }
