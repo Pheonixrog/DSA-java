10cbaf4131606e7ae13b622c75c7b1f7bf80a86e
Rishabh Katiyar <rishabhasus9@gmail.com>
2023-12-17 20:14:46 +0530

    factorial and summation function by recursion

--- 4. recursion/factorial.java
+++ 4. recursion/factorial.java
@@ -1,3 +1,32 @@
+import java.util.*;
+
 public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int a = sc.nextInt();
+      
+        System.out.println(fact(a));
+        System.out.println(summation(a));
+        
+
+    }
+
+    public static int fact(int a){
+        if(a==0){
+            return 1;
+        }
+
+        return a * fact(a-1);
+
+    }
     
+    public static int summation(int a){
+        if(a==0){
+            return 0;
+        }
+
+        return a + summation(a-1);
+
+    }
 }
