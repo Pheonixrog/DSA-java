c48f5ee4a44f5befa67544a4d3636adecf62dd09
Rishabh Katiyar <rishabhasus9@gmail.com>
2023-12-23 23:18:29 +0530

    update

--- 4. recursion/fibo.java
+++ 4. recursion/fibo.java
@@ -1,20 +1,19 @@
 public class fibo {
     public static void main(String[] args) {
-        int n =25;
+        int n = 25;
         System.out.println(fibonacci(n));
-        
+
     }
 
     public static int fibonacci(int n) {
-        if(n==1){
+        if (n == 1) {
             return 1;
         }
-        if(n==0){
+        if (n == 0) {
             return 0;
         }
 
-        return fibonacci(n-1) + fibonacci(n-2);
-        
+        return fibonacci(n - 1) + fibonacci(n - 2);
 
     }
 }
